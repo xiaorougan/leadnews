514ed2069e12d86be9e02072051bfab9ad232043
xiaorougan <1749059433@qq.com>
2024-01-08 23:35:19 +0800

    pc change1

--- heima-leadnews-test/minio-demo/src/test/java/com/minio/MinIOTest.java
+++ heima-leadnews-test/minio-demo/src/test/java/com/minio/MinIOTest.java
@@ -8,17 +8,17 @@ import java.io.FileOutputStream;
 public class MinIOTest {
     public static void main(String[] args) {
         //获取minio的链接信息 创建一个minio的客户端
-        FileOutputStream fileOutputStream = new FileOutputStream()
-        MinioClient minioClient = MinioClient.builder().credentials("minio", "minio123").endpoint("http://10.21.172.231:9000").build();
-
-
-        //上传
-        PutObjectArgs putObjectArgs =PutObjectArgs.builder()
-                .object("list.html")
-                .contentType("test/html")
-                .bucket("news")
-                .stream();
-        minioClient.putObject(putObjectArgs);
+//        FileOutputStream fileOutputStream = new FileOutputStream()
+//        MinioClient minioClient = MinioClient.builder().credentials("minio", "minio123").endpoint("http://10.21.172.231:9000").build();
+//
+//
+//        //上传
+//        PutObjectArgs putObjectArgs =PutObjectArgs.builder()
+//                .object("list.html")
+//                .contentType("test/html")
+//                .bucket("news")
+//                .stream();
+//        minioClient.putObject(putObjectArgs);
 
     }
 }
