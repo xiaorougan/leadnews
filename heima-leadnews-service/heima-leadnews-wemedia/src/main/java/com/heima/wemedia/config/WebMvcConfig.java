8c66076388301d025598802d49a1317892350001
xiaorougan_note <17490594333@qq.com>
2024-01-11 15:59:28 +0800

    change 23/1/11 16:00 commit

---
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/config/WebMvcConfig.java
@@ -0,0 +1,12 @@
+package com.heima.wemedia.config;
+
+import com.heima.wemedia.interceptor.WmTokenInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new WmTokenInterceptor()).addPathPatterns("/**");
+    }
+}
